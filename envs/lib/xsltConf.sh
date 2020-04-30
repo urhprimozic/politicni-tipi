10b0e58b85d3c7d3ccf9e945a5d5b66f2bfd93b7
urh <urh.primozic@gmail.com>
2020-04-30 17:28:46 +0200

    Upload envs folder - okolje za condo

---
+++ envs/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/mnt/Podatki/Programiranje/politicni-tipi/politicni-tipi/envs/lib"
+XSLT_LIBS="-lxslt -L/mnt/Podatki/Programiranje/politicni-tipi/politicni-tipi/envs/lib -lxml2 -L/mnt/Podatki/Programiranje/politicni-tipi/politicni-tipi/envs/lib -lz -L/mnt/Podatki/Programiranje/politicni-tipi/politicni-tipi/envs/lib -llzma -lpthread -L/mnt/Podatki/Programiranje/politicni-tipi/politicni-tipi/envs/lib -liconv -L/mnt/Podatki/Programiranje/politicni-tipi/politicni-tipi/envs/lib -licui18n -licuuc -licudata -lm -ldl -lm -lrt"
+XSLT_INCLUDEDIR="-I/mnt/Podatki/Programiranje/politicni-tipi/politicni-tipi/envs/include"
+MODULE_VERSION="xslt-1.1.33"
